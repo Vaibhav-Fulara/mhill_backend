5fe5ab3e12f771a376e61df13345692915b346b1
Rudraksh <rudrakshsuper2001@gmail.com>
2022-11-10 22:06:12 +0530

    Photo to photos

--- models/Trip.js
+++ models/Trip.js
@@ -15,7 +15,7 @@ const TripSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    photo: String,
+    photos: [String],
     price:{
         type:Number,
         required:true
